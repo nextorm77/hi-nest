f6af442f299d80bf570fec323d7acae5ae6a51ed
nextomr77 <sonsw77@gmail.com>
2022-06-25 22:34:48 +0900

    tested 2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,36 +8,42 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { query } from 'express';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies.';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
+  /*
   @Get('search')
   search(@Query('year') searchingYear: string) {
     // 요청: ~/movies/search?year=2000
     return `We are searching for a movie made after: ${searchingYear}`;
   }
+  */
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}.`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
     // Body > JSON 데이터 전송: {"name":"Tenet","director":"Nolan"}
     //console.log(movieData);
-    return movieData; // express와 달리 JSON 중간조작없이 출력
+    // return movieData; // express와 달리 JSON 중간조작없이 출력 가능
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { title } from 'process';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    // parseInt(id) -> +id: string을 number로 변환
+    return this.movies.find((movie) => movie.id === +id);
+  }
+
+  deleteOne(id: string): boolean {
+    this.movies.filter((movie) => movie.id !== +id);
+    return true;
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
