b528e6ee26a5c9baaa5774eade4beab682ce5cab
nextomr77 <sonsw77@gmail.com>
2022-06-27 12:15:56 +0900

    tested 2.5 DTOs and Validation part Two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -53,7 +54,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { title } from 'process';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -32,7 +33,7 @@ export class MoviesService {
     });
   }
 
-  update(id: number, updateData) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     // "MDN 객체 리터럴에서의 전개" 참고
